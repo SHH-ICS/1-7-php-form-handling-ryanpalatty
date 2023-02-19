e640dcc9f1f1dea19c43633bfce8496f0e4a2564
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-02-19 13:15:45 -0500

    Create handler.php

---
+++ handler.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Page Title</title>
+  </head>
+
+  <body>
+    
+    <?php
+    echo "<h1>Welcome ".$_POST['userName']."!</h1>\n";
+    ?>
+    
+  </body>
+  
+</html>
